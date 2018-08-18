6c511c0ccfd5e1ebd2ed803460e12f283899fa5a
hisbmitchell <heathermitchell.ottawa@gmail.com>
2018-08-18 15:38:25 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -22,26 +22,27 @@ function setup() {
   
 
   videoPlacementX = windowWidth * 0.45; 
-  videoPlacementY = windowHeight * 0.25; 
+  videoPlacementY = windowHeight * 0.35;
   
   textAlign(RIGHT, TOP);
-  textSize(32);
+  textSize(38);
   textStyle(BOLD);
   text('VAULT', videoPlacementX - 30, videoPlacementY);
   textStyle(NORMAL);
   textSize(12);
-  text('this is the time and location etc', videoPlacementX - 30, videoPlacementY + 45);
-  text("here's some more info", videoPlacementX - 30, videoPlacementY + 65);
-  text('another info', videoPlacementX - 30, videoPlacementY + 85);
-  text('info info info', videoPlacementX - 30, videoPlacementY + 105);
-   
+  text('This is the time and location etc', videoPlacementX - 30, videoPlacementY + 45);
+  text("Here's some more info", videoPlacementX - 30, videoPlacementY + 65);
+  text('Another info', videoPlacementX - 30, videoPlacementY + 85);
+  text('Info info info', videoPlacementX - 30, videoPlacementY + 105);
+ 
+  text('there could also be some text here which could be nice', videoPlacementX + video.width, videoPlacementY + video.height + 10);
    
 }
 
 function draw() {
   //background('#e5000');
-  videoPlacementX = windowWidth * 0.5; 
-  videoPlacementY = windowHeight * 0.25; 
+  videoPlacementX = windowWidth * 0.45; 
+  videoPlacementY = windowHeight * 0.35; 
  
  image(video, videoPlacementX , videoPlacementY, 320, 240);
 
@@ -52,11 +53,14 @@ function draw() {
 
 function windowResized(){
         canvas = resizeCanvas(windowWidth, windowHeight);
-        text('VAULT',  videoPlacementX - 30, videoPlacementY);
-         textSize(12);
-        text('this is the time and location etc', videoPlacementX - 30, videoPlacementY + 35);
-        text("here's some more info", videoPlacementX - 30, videoPlacementY + 55);
-        text('another info', videoPlacementX - 30, videoPlacementY + 75);
-        text('info info info', videoPlacementX - 30, videoPlacementY + 95);
-        //bgVideo.size(windowHeight*2, 300);
+         textAlign(RIGHT, TOP);
+  textSize(38);
+  textStyle(BOLD);
+  text('VAULT', videoPlacementX - 30, videoPlacementY);
+  textStyle(NORMAL);
+  textSize(12);
+  text('This is the time and location etc', videoPlacementX - 30, videoPlacementY + 45);
+  text("Here's some more info", videoPlacementX - 30, videoPlacementY + 65);
+  text('Another info', videoPlacementX - 30, videoPlacementY + 85);
+  text('Info info info', videoPlacementX - 30, videoPlacementY + 105);
       }
