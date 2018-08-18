1e2c6a707b36b6397fb5aed57037f5d2c1dfa705
hisbmitchell <heathermitchell.ottawa@gmail.com>
2018-08-17 21:52:31 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,32 +1,50 @@
-var capture;
-var canvas;
+var video;
+
 
 function setup() {
-   canvas = createCanvas(windowWidth, windowHeight);
+   canvas = createCanvas(640, 480, WEBGL);
+  background('#e50000');
    canvas.id('p5canvas');
-   background('#bc0319');
-  capture = createCapture(VIDEO);
-  capture.size(320, 240);
-  capture.id('p5video');
+   
+   
+   
+  // rectangle = rect(320, 240);
+  // rectangle.id('rectangle');
+   
+  video = createCapture(VIDEO);
+  video.size(320, 240);
+  video.hide();
+  video.id('p5video');
+  
   
   var seriously = new Seriously();
   
   var src = seriously.source('#p5video');
   var target = seriously.target('#p5canvas');
   
+  var grain = seriously.effect('filmgrain');
+  grain.amount = 0.1;
+  
+  grain.source = src;
+  target.source = grain;
+  
   seriously.go();
+  
+  canvas.size(320, 240);
+   
    
 }
 
 function draw() {
-  background('#bc0319');
-  image(capture, windowWidth * 0.5, windowHeight * 0.25, 320, 240);
-  //filter('INVERT');
+  canvas.position(windowWidth * 0.55, windowHeight * 0.3);
+ // background('#bc0319');
+  //image(video, windowWidth * 0.5, windowHeight * 0.25, 320, 240);
+  // //filter('INVERT');
     
   
 }
 
- function windowResized(){
-        resizeCanvas(windowWidth, windowHeight);
+function windowResized(){
+        canvas.position(windowWidth * 0.6, windowHeight * 0.4);
         //bgVideo.size(windowHeight*2, 300);
       }
